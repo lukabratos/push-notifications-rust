78cbae4a09ac71dfcaba6e1600720dd8a0677a77
Luka Bratos <luka.bratos@gmail.com>
2018-07-11 00:04:34 +0100

    Create Payload struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 #[macro_use]
 extern crate ureq;
 
+#[macro_use]
+extern crate serde_derive;
+
 use std::collections::HashMap;
+
+#[derive(Serialize)]
+struct Payload {
+    interests: Vec<String>,
+    publish_request: HashMap<String, HashMap<String, HashMap<String, String>>>,
+}
 
 pub fn publish(instance_id: String, secret_key: String, interests: Vec<String>, publish_request: HashMap<String, HashMap<String, HashMap<String, String>>>) {
 
@@ -9,13 +18,18 @@ pub fn publish(instance_id: String, secret_key: String, interests: Vec<String>, 
     println!("{:?}", interests);
     println!("{:?}", publish_request);
 
-    // Merge `interests` into the `publish_request`.
+    let payload = Payload {
+        interests: interests,
+        publish_request: publish_request
+    };
+
+    println!("{:?}", json!(payload));
 
     let resp = ureq::post(format!("https://{}.pushnotifications.pusher.com/publish_api/v1", instance_id))
     .set("Accept", "application/json")
     .set("Content-Type", "application/json")
     .set("Authorization", format!("Bearer {}", secret_key))
-    .send_json(json!(publish_request));
+    .send_json(json!(payload));
 
     if resp.ok() {
         println!("{:?}", resp);
