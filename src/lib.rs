fdafbeebf7b83e989c61131271f2dde56a8460cd
Luka Bratos <luka.bratos@gmail.com>
2018-07-10 19:07:28 +0100

    Implement a very basic POST request

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,15 @@
-extern crate futures;
-extern crate hyper;
-extern crate tokio_core;
-
-use futures::{Future};
-use hyper::{Client, Uri};
-use tokio_core::reactor::Core;
+#[macro_use]
+extern crate ureq;
 
 pub fn publish() {
-    let mut core = Core::new().unwrap();
-    let client = Client::new(&core.handle());
+    let resp = ureq::post("http://httpbin.org/post")
+    .set("X-Luka-Header", "Test")
+    .send_json(json!({
+        "a": "aaaa",
+        "b": "bbbb"
+    }));
 
-    let url : Uri = "http://httpbin.org/response-headers?foo=bar".parse().unwrap();
-    assert_eq!(url.query(), Some("foo=bar"));
-
-    let request = client.get(url)
-        .map(|res| {
-            println!("{}", res.status());
-            assert_eq!(res.status(), hyper::Ok);
-        });
-
-    core.run(request).unwrap();
+    if resp.ok() {
+        println!("{:?}", resp);
+    }
 }
