24bb975a5dc363853f4407293d66d651fe2b5fd2
Luka Bratos <luka.bratos@gmail.com>
2018-07-10 20:07:14 +0100

    Add test interests and payload

--- src/bin.rs
+++ src/bin.rs
@@ -2,6 +2,19 @@ extern crate beams;
 
 use beams::publish;
 
+use std::collections::HashMap;
+
 pub fn main() {
-    publish();
+    let interests = vec![String::from("pizza"), String::from("avocado")];
+
+    let mut alert = HashMap::new();
+    alert.insert(String::from("alert"), String::from("hi"));
+
+    let mut aps = HashMap::new();
+    aps.insert(String::from("aps"), alert);
+    
+    let mut publish_request = HashMap::new();
+    publish_request.insert(String::from("apns"), aps);
+
+    publish(interests, publish_request);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 #[macro_use]
 extern crate ureq;
 
-pub fn publish() {
+use std::collections::HashMap;
+
+pub fn publish(interests: Vec<String>, publish_request: HashMap<String, HashMap<String, HashMap<String, String>>>) {
+
+    println!("{:?}", interests);
+    println!("{:?}", publish_request);
+
     let resp = ureq::post("http://httpbin.org/post")
     .set("X-Luka-Header", "Test")
     .send_json(json!({
